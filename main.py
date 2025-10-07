954779d00c843d40d01a15e69c4b63695d2fc3be
Dato Bagauri <bagauridato16@gmail.com>
2025-10-07 17:24:47 +0400

    ragaca

---
+++ main.py
@@ -0,0 +1,17 @@
+def func(num):
+    if num%2==0:
+        print(True)
+    else:
+        print(False)
+
+
+def sia(siaa):
+    total=0
+    for i in siaa:
+        total+=i
+    print(total)
+
+listik=[2,3,4,5,6,7]
+
+func(4)
+sia(listik)
